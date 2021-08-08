ac73e85e93a93aa29b6edfce8011959ede1ff28e
ariel0123 <llamas0123@hotmail.com>
2021-08-08 14:28:10 -0700

    Initial Commit

---
+++ pokedexApp/pokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexAppApp.swift
+//  pokedexApp
+//
+//  Created by Ariel Ortiz on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct pokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
